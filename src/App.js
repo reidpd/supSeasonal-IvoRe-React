a9a3206bc0980d3fcd515d14c8abc159ecd0255d
Ivonne Terrero <iqcodes21@gmail.com>
2017-04-05 00:22:59 -0700

    changes to Login component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+// import ReactDOM from 'react-dom';
+import Login from './components/navBar';
+// import logo from './logo.svg';
+// import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recipes: []
+    }
+
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>supSeasonal</h2>
+            <Login />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
 
-export default App;
+
+export default App
---
+++ src/components/navBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Navbar} from 'react-bootstrap';
+
+// class NavBar extends Component {
+//   render () {
+//     return (
+//     <Navbar >
+//       <NavItem>supSeasonal</NavItem>
+//     </Navbar>
+//
+//     )
+//   }
+// }
+
+const Login = () => {
+  return (
+    <Navbar.Text pull-right>
+      <a>LogIn</a>
+    </Navbar.Text>
+  )
+}
+
+export default Login;
