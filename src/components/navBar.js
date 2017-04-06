dede1d86565b6c645db7b3d7477f0211e9cb157e
Ivonne Terrero <iqcodes21@gmail.com>
2017-04-06 08:51:43 -0700

    minimal changes

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-import { Navbar} from 'react-bootstrap';
+import {Navbar} from 'react-bootstrap';
+
+class NavBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      month: ''
+    }
+  }
+  render(){
+    return (
+      // <div>
+      //   would like to render an image associated with the time of the month.
+      //   might need to be a separate component.
+      // </div>
+      <Navbar>
+        <Navbar.Header>
+            <Navbar.Brand>
+              <div>
+              <a href="#">React-Bootstrap</a>
+            </div>
+            </Navbar.Brand>
+          </Navbar.Header>
+      </Navbar>
+    )
+  }
+}
 
 // class NavBar extends Component {
 //   render () {
@@ -12,6 +38,27 @@ import { Navbar} from 'react-bootstrap';
 //     )
 //   }
 // }
+//
+// const navbarInstance = (
+//   <Navbar>
+//     <Navbar.Header>
+//       <Navbar.Brand>
+//         <a href="#">React-Bootstrap</a>
+//       </Navbar.Brand>
+//     </Navbar.Header>
+//     <Nav>
+//       <NavItem eventKey={1} href="#">Link</NavItem>
+//       <NavItem eventKey={2} href="#">Link</NavItem>
+//       <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+//         <MenuItem eventKey={3.1}>Action</MenuItem>
+//         <MenuItem eventKey={3.2}>Another action</MenuItem>
+//         <MenuItem eventKey={3.3}>Something else here</MenuItem>
+//         <MenuItem divider />
+//         <MenuItem eventKey={3.4}>Separated link</MenuItem>
+//       </NavDropdown>
+//     </Nav>
+//   </Navbar>
+// )
 
 const Login = () => {
   return (
@@ -21,4 +68,4 @@ const Login = () => {
   )
 }
 
-export default Login;
+export default NavBar;
--- src/components/single_ingredient.js
+++ src/components/single_ingredient.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const IngredientListItem = (props) => {
+const IngredientListItem = () => {
   return (
 
   )
