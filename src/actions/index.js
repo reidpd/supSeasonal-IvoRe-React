5317f65f633b2cb4198806b6471675cf292d58a9
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-04-08 15:16:51 -0700

    added initialState

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
 import React, { Component } from 'react';
+
+export const action1 = (props) => {
+  return {
+    type: 'ACTION_1',
+    props
+  }
+}
+
+export const action2 = (props) => {
+  const data = /* API Fetch */;
+  return {
+    type: 'ACTION_2',
+    payload: data
+  }
+}
