ee86dd907f7837dde1fa997a18dafb4e4e2a98d6
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-04-08 16:08:54 -0700

    manipulating navbar component

--- src/components/MonthDropdown.js
+++ src/components/MonthDropdown.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+
+import {  }
+
 import {NavDropdown, MenuItem, Nav} from 'react-bootstrap';
 
 const Months = () => {
@@ -6,7 +9,13 @@ const Months = () => {
   return (
   <div>
     <Nav>
-      <NavDropdown eventKey={3} title="Months" id="basic-nav-dropdown">
+      <NavDropdown
+        eventKey={3}
+        title="Months"
+        id="basic-nav-dropdown"
+        onChange={(event) => {
+          this.props.setMonth(event.target.value);
+        }}>
         <MenuItem eventKey={3.1}> January</MenuItem>
         <MenuItem eventKey={3.1}> February</MenuItem>
         <MenuItem eventKey={3.1}> March</MenuItem>
